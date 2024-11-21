7a833fa825e8823d859757ba0744f003cd0b7ab7
Lucien <lucien.boisseau-sable@smice.com>
2024-11-22 00:13:50 +0100

    fix nuxt.config: sanity api version issue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,61 +1,72 @@
 export default defineNuxtConfig({
-	devtools: { enabled: true },
-	css: ['@/assets/style/global.scss'],
+  devtools: { enabled: true },
+  css: ['@/assets/style/global.scss'],
 
-	vite: {
-		css: {
-			preprocessorOptions: {
-				scss: {
-					api: 'modern-compiler',
-					additionalData: '@use "~/assets/style/_variables.scss" as *;',
-				},
-			},
-		},
-	},
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler',
+          additionalData: '@use "~/assets/style/_variables.scss" as *;',
+        },
+      },
+    },
+  },
 
-	modules: [
-		'@pinia/nuxt',
-		'@nuxtjs/i18n',
-		[
-			'@nuxtjs/google-fonts',
-			{
-				families: {
-					Lato: [500, 700, 800],
-					download: true,
-					inject: true,
-				},
-			},
-		],
-	],
-	i18n: {
-		locales: [
-			{
-				code: 'en',
-				language: 'en-US',
-				file: 'en-US.ts',
-			},
-			{
-				code: 'fr',
-				language: 'fr-FR',
-				file: 'fr-FR.ts',
-			},
-		],
-		detectBrowserLanguage: {
-			useCookie: true,
-			cookieKey: 'i18n_redirected',
-			cookieSecure: false,
-		},
-		strategy: 'no_prefix', // don't add language to url
-	},
+  modules: [
+    '@pinia/nuxt',
+    '@nuxtjs/i18n',
+    [
+      '@nuxtjs/google-fonts',
+      {
+        families: {
+          Lato: [500, 700, 800],
+          download: true,
+          inject: true,
+        },
+      },
+    ],
+  ],
+  i18n: {
+    locales: [
+      {
+        code: 'en',
+        language: 'en-US',
+        file: 'en-US.ts',
+      },
+      {
+        code: 'fr',
+        language: 'fr-FR',
+        file: 'fr-FR.ts',
+      },
+    ],
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_redirected',
+      cookieSecure: false,
+    },
+    strategy: 'no_prefix', // don't add language to url
+  },
 
-	components: [
-		{
-			path: '~/components/_UI',
-			pathPrefix: false,
-			prefix: 'UI',
-		},
-		'~/components',
-	],
+  components: [
+    {
+      path: '~/components/_UI',
+      pathPrefix: false,
+      prefix: 'UI',
+    },
+    '~/components',
+  ],
+  sanity: {
+    projectId: process.env.NUXT_SANITY_PROJECT_ID,
+    dataset: process.env.NUXT_SANITY_DATASET,
+    useCdn: true, // `false` if you want to ensure fresh data
+    // apiVersion: process.env.NUXT_SANITY_API_VERSION || '2021-03-25',
+    visualEditing: {
+      studioUrl: process.env.NUXT_SANITY_STUDIO_URL || 'http://localhost:3333',
+      token: process.env.NUXT_SANITY_API_READ_TOKEN,
+      stega: true,
+    },
+  },
 
-	compatibilityDate: '2024-11-16',
+  compatibilityDate: '2024-11-16',
 })
