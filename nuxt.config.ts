3539dba075806d466d888a578978b20d7d547e7b
Lucien <lucien.boisseau-sable@smice.com>
2024-11-21 17:47:28 +0100

    removing package-lock; removing sanity files;

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,6 @@ export default defineNuxtConfig({
   },
 
   modules: [
-    // '@nuxtjs/sanity',
     '@pinia/nuxt',
     [
       '@nuxtjs/google-fonts',
@@ -47,18 +46,6 @@ export default defineNuxtConfig({
       },
     ],
   ],
-
-  // sanity: {
-  //   projectId: process.env.NUXT_SANITY_PROJECT_ID,
-  //   dataset: process.env.NUXT_SANITY_DATASET,
-  //   useCdn: true, // `false` if you want to ensure fresh data
-  //   apiVersion: process.env.NUXT_SANITY_API_VERSION || '2024-03-15',
-  //   visualEditing: {
-  //     studioUrl: process.env.NUXT_SANITY_STUDIO_URL || 'http://localhost:3333',
-  //     token: process.env.NUXT_SANITY_API_READ_TOKEN,
-  //     stega: true,
-  //   },
-  // },
 
   components: [
     {
